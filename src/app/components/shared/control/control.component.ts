0cf11e70fda5dbeac4ba8d29d0e29375a88cef05
guy arbily <guy.arbilly@gmail.com>
2025-02-22 18:31:38 +0200

    chapter 6 section 115 - workig with  @HostBinding and @HostListener

--- src/app/components/shared/control/control.component.ts
+++ src/app/components/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, Host, input, ViewEncapsulation } from '@angular/core';
+import { Component, Host, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,10 +8,22 @@ import { Component, Host, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None, // insure that current style will become global
   host: {
-    class: 'control'
+    class: 'control',
+    '(click)': 'onClick()' // part of the 2nd way for listen to host events - in this case click that will trigger the onClick method
   }
 })
 export class ControlComponent {
+  // alternaltive for setting host uinsg code with @HostBinding devorator - but this is the old way
+  // @HostBinding('class') className = 'control' // insure that current style will become global with setting clsss= control on the host element
   label = input.required<string>()
+  // we have also option to listen to host events using @HostListener or via adding properties to the host
+  // @HostListener('click') onClick() {
+  //   console.log('click')
+  // }
+
+  // 2nd way to use host attributes it so have the onclick method and use the event click atribute to connect tothe function
+  onClick() {
+    console.log('click')
+  }
 
 }
