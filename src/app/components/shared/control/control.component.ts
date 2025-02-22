a2e16b750cf43e3ea3dd6ae9ef47c6f2538a2042
guy arbily <guy.arbilly@gmail.com>
2025-02-22 15:25:05 +0200

    chapter 6 section 113 - adding host to control winstead p with class=control

--- src/app/components/shared/control/control.component.ts
+++ src/app/components/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, Host, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -6,7 +6,10 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None // insure that current style will become global
+  encapsulation: ViewEncapsulation.None, // insure that current style will become global
+  host: {
+    class: 'control'
+  }
 })
 export class ControlComponent {
   label = input.required<string>()
